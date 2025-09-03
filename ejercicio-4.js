6eaa47858e052288170bdf08917090b676fbf2be
Nattalic <nordonez376@gmail.com>
2025-09-02 20:20:31 -0500

    a

--- ejercicio-4.js
+++ ejercicio-4.js
@@ -5,13 +5,8 @@ const li = document.querySelectorAll("#nombres li")
 
 // Paso 2: Escucha el evento input
 
-input.addEventListener('input' , => {
-
-}
-
-)
-
-
+input.addEventListener('click', () => {
+})
 
 
 // Paso 3: Filtra los nombres mostrando solo los que coincidan con lo escrito
